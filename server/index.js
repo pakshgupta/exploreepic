f9c5e34af7eca73beecfe89297e79c94985610a6
Paksh <pakshgupta007@gmail.com>
2024-05-02 20:47:54 +0530

    add cors

--- server/index.js
+++ server/index.js
@@ -7,9 +7,10 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
+const cors=require("cors");
 const multer = require("multer");
 const path = require("path");
-
+app.use(cors());
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 mongoose
